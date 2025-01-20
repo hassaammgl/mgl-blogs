22104c26c4697a683c7eb960d2ac0afe5ff62537
hassaammgl <h455a4mmehtab@gmail.com>
2025-01-20 17:12:20 +0500

    add loading components for various pages

---
+++ app/(userAdmin)/user/loading.jsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+	return (
+		<div className="container mx-auto mt-12 p-8">
+			<h1 className="text-4xl font-bold text-center mb-8 text-gray-900 dark:text-white">
+				Loading...
+			</h1>
+		</div>
+	);
+}
