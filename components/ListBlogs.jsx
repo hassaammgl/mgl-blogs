e37e3a357f706fcfc913aaabfcba45620d421b60
hassaammgl <h455a4mmehtab@gmail.com>
2025-01-29 10:30:54 +0500

    refactor GradientCard component for improved layout and enhanced blog details display; update image handling and add read time and creation date

--- components/ListBlogs.jsx
+++ components/ListBlogs.jsx
@@ -40,12 +40,15 @@ const GradientCard = ({ blog }) => {
 	}
 
 	return (
-		<div className="group bg-white dark:bg-gray-800 rounded-2xl shadow-xl hover:shadow-2xl transform hover:-translate-y-2 transition-all duration-200 flex flex-row h-40 w-full ">
-			<div className="relative overflow-hidden rounded-t-2xl">
-				<img
+		<div className="group bg-white dark:bg-gray-800 rounded-xl my-4 shadow-xl hover:shadow-2xl transform hover:-translate-y-2 transition-all duration-200 flex flex-row h-52 w-full ">
+			{/* image section */}
+			<div className="relative overflow-hidden rounded-l-xl flex justify-center items-center">
+				<Image
 					src={blog.image}
+					width={300}
+					height={300}
 					alt={blog.title}
-					className="w-full h-48 object-cover transform group-hover:scale-110 transition-transform duration-200"
+					className="object-fill transform group-hover:scale-110 transition-transform duration-200"
 				/>
 				{blog.createdAt &&
 					moment().diff(moment(blog.createdAt), "days") <= 7 && (
@@ -54,45 +57,66 @@ const GradientCard = ({ blog }) => {
 						</div>
 					)}
 			</div>
-			<div className="p-6 flex">
+			{/* data section */}
+			<div className="flex flex-col justify-between p-4">
+				<div>
+					<h3 className="text-2xl font-bold text-gray-800 dark:text-white mb-2">
+						{blog.title}
+					</h3>
+					<p className="text-gray-600 dark:text-gray-400">
+						{blog.description.length > 100
+							? `${blog.description.substring(0, 100)}...`
+							: blog.description}
+					</p>
+				</div>
 
-				{/* Display title and description */}
-				<h3 className="text-xl font-bold text-gray-900 dark:text-white mt-2">
-					{blog.title}
-				</h3>
-				<p className="mt-3 text-gray-600 dark:text-gray-300 line-clamp-3">
-					{blog.description}
-				</p>
-				<span className="text-blue-600 dark:text-blue-400 text-sm font-semibold">
-					{blog.category}
-				</span>
-				<span className="float-right text-blue-600 dark:text-blue-400 text-sm font-semibold">
-					{getReadTime(blog.content)}
-				</span>
-				<div className="flex items-center mt-4">
-					<img
-						src={blog.author.imageUrl}
-						alt={`${blog.author.firstName} Avatar`}
-						className="w-8 h-8 rounded-full"
-					/>
-					<div className="ml-3">
-						<p className="text-sm font-semibold text-gray-900 dark:text-white">
-							{blog.author.firstname + " " + blog.author.lastname}
-						</p>
-						<p className="text-sm text-gray-500 dark:text-gray-400">
-							{moment(blog.createdAt).fromNow()}
+				<div className="flex justify-between items-center mt-4">
+					<div className="flex items-center">
+						<div className="flex items-center mr-4">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								className="h-6 w-6 text-gray-600 dark:text-gray-400"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke="currentColor"
+							>
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									strokeWidth={2}
+									d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+								/>
+							</svg>
+							<p className="text-gray-600 dark:text-gray-400">
+								{getReadTime(blog.content)}
+							</p>
+						</div>
+						<div className="flex items-center">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								className="h-6 w-6 text-gray-600 dark:text-gray-400"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke="currentColor"
+							>
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									strokeWidth={2}
+									d="M13 10V3L4 14h7v7l9-11h-7z"
+								/>
+							</svg>
+							<p className="text-gray-600 dark:text-gray-400">
+								{moment(blog.createdAt).format("MMM Do YYYY")}
+							</p>
+						</div>
+					</div>
+					<div className="flex items-center">
+						<p className="text-gray-600 dark:text-gray-400">
+							{blog.category}
 						</p>
 					</div>
 				</div>
-				{/* Display view count */}
-				<p className="mt-2 text-gray-500 dark:text-gray-400 text-sm">
-					{blog.viewCount} {blog.viewCount === 1 ? "view" : "views"}
-				</p>
-				<Link href={`/blog/${blog._id}`}>
-					<button className="w-full px-4 md:px-6 py-3 md:py-4 bg-gradient-to-r from-blue-500 to-blue-600 text-white rounded-xl font-bold hover:opacity-90 transform hover:-translate-y-1 transition-all duration-200 mt-4">
-						Read More
-					</button>
-				</Link>
 			</div>
 		</div>
 	);
