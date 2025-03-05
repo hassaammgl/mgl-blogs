51d13db66d2f466d5c69ac72bd4a63d19df9f39c
hassaammgl <h455a4mmehtab@gmail.com>
2025-03-05 16:21:55 +0500

    feat: enhance image generation API with error handling and save images to public/tmp

--- src/app/(api)/api/img-gen/route.js
+++ src/app/(api)/api/img-gen/route.js
@@ -1,34 +1,33 @@
 import fs from 'fs';
+import path from 'path';
 import fetch from 'node-fetch';
 import { NextResponse } from 'next/server';
 
 
 async function downloadImage(imageUrl) {
     const response = await fetch(imageUrl);
-    const buffer = await response.buffer();
-    fs.writeFileSync('', buffer);
+    const arrayBuffer = await response.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+    const filePath = path.join(process.cwd(), 'public', 'tmp', `flux-${Date.now().toLocaleString()}.png`);
+    fs.writeFileSync(filePath, buffer);
     console.log('Download Completed');
+    return filePath;
 }
 
-// // Image details
-// const prompt = 'A beautiful landscape';
-// const width = 1024;
-// const height = 1024; 
-// const seed = 42; // Each seed generates a new image variation
-// const model = 'flux'; // Using 'flux' as default if model is not provided
-
-// const imageUrl = `https://pollinations.ai/p/${encodeURIComponent(prompt)}?width=${width}&height=${height}&seed=${seed}&model=${model}`;
-
-// downloadImage(imageUrl);
-
 export const POST = async (req) => {
-    const { prompt, width, height, seed, model } = await req.body;
-    
+    const { prompt, width, height, seed, model } = await req.json();
 
+    if (!prompt || width < 200 || height < 200 || seed < 20) {
+        return NextResponse.json({ error: 'All fields is required' }, { status: 400 });
+    }
 
-    const imageUrl = `https://pollinations.ai/p/${encodeURIComponent(prompt)}?width=${width}&height=${height}&seed=${seed}&model=${model}`;
-    console.log(prompt, width, height, seed, model);
-
-    downloadImage(imageUrl);
-    return NextResponse.json({ imageUrl });
+    try {
+        const imageUrl = `https://pollinations.ai/p/${encodeURIComponent(prompt)}?width=${width || 1024}&height=${height || 1024}&seed=${seed || 761}&model=${model || "flux"}`;
+        console.log(prompt, width, height, seed, model);
+        const path = downloadImage(imageUrl);
+        return NextResponse.json({ imageUrl, path }, { status: 200 });
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ error: 'An error occurred' }, { status: 500 });
+    }
 }
